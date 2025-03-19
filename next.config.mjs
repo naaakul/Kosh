d2f0c5ea6d43d9e880517453b7d6d46e3ce07cde
Nakul Chouksey <71.nakul@gmail.com>
2025-03-19 14:21:22 +0200

    opt: increase max chunk size

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,11 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   disable: false,
+  maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
 });
 
 export default withPWA({
   output: "export",
   distDir: "./dist",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['@aptos-labs/wallet-adapter-react']
-  }
 });
